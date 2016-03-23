9c619a60297c8e9a9e4c984ed8da120f83b21e99
Ben <ben@Mario.fios-router.home>
2016-03-23 19:30:43 -0400

    My first commit

---
+++ Bullseye/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Bullseye
+//
+//  Created by Ben on 3/16/16.
+//  Copyright © 2016 The Finked Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    } //puntfunt191010101001101010101110 <-(this is my(what i thought)was a codish languge)
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
+        alert.addAction(action)
+
+        presentViewController(alert, animated: true, completion: nil)
+    }
+}
+
